bb02e0928b55abd018632335bb59629069d9e230
Gustavo Carvalho Silva <gustavocrvl42@gmail.com>
2021-08-01 15:27:40 -0300

    favorites list

--- src/screens/Favorites/index.tsx
+++ src/screens/Favorites/index.tsx
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Text, View } from "react-native";
+import { FlatList } from 'react-native-gesture-handler';
 import { BottomNavigation } from '../../components/BottomNavigation';
 import { NoteCard } from '../../components/NoteCard';
+import { db } from '../../database/connection';
 import { styles } from './styles';
 
+type Note = {
+  id: number;
+  title: string;
+  content: string;
+  is_starred: boolean;
+}
+
 export function Favorites() {
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    db.transaction((tx) => {
+      tx.executeSql("select * from notes where is_starred = true", [], (_, { rows: { _array } }: any) => {
+        setNotes(_array)
+      });
+    })
+  }, [])
+
   return (
     <View style={styles.container}>
       <View style={{ flex: 1, width: '100%', padding: 10 }}>
-        <Text style={{ fontSize: 26 }}>
+        <Text style={{ fontSize: 26, paddingVertical: 20 }}>
           Favoritas
         </Text>
-        <NoteCard
-          title="Nota de Teste"
-          subtitle="tetststsasfjsadfksdajfkasdjkfjkafklsdjfasdjfsdjfdsjfsdjfjkdsfkjsdfjksdkfj"
+        <FlatList
+          data={notes}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => <NoteCard title={item.title} subtitle={item.content} />}
         />
       </View>
       <BottomNavigation active="Favorites" />
